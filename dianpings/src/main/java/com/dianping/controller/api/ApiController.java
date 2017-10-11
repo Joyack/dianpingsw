d1d7fc1c7f41a3fe336633832e32b6f7fabed688
Joyack <497089251@qq.com>
2017-10-11 11:33:48 +0800

    啊

---
+++ dianpings/src/main/java/com/dianping/controller/api/ApiController.java
@@ -0,0 +1,22 @@
+package com.dianping.controller.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController
+{
+    @RequestMapping(value = "/homead",method = RequestMethod.GET)
+    public List<Ad> homead(){
+
+        ObjectMapper mapper = new ObjectMapper();
+        String s
+
+                retu
+    }
+}
